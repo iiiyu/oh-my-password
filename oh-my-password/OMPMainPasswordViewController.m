6e703393c311f7e5abc43a4db9eb7d7d6fbc91f2
Xiao ChenYu <yunsn0303@gmail.com>
2012-04-20 14:53:18 +0800

    add iphone's main password view and view controller

---
+++ oh-my-password/OMPMainPasswordViewController.m
@@ -0,0 +1,43 @@
+//
+//  OMPMainPasswordViewController.m
+//  oh-my-password
+//
+//  Created by ChenYu Xiao on 12-4-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "OMPMainPasswordViewController.h"
+
+@interface OMPMainPasswordViewController ()
+
+@end
+
+@implementation OMPMainPasswordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
