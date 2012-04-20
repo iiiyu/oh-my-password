fe2bbe0dba88eeffa4b9c6193aefe588dbbeebeb
Xiao ChenYu <yunsn0303@gmail.com>
2012-04-20 14:49:15 +0800

    add iphone add main user view and add main user view controller file

---
+++ oh-my-password/OMPAddMainUserViewController.m
@@ -0,0 +1,43 @@
+//
+//  OMPAddMainUserViewController.m
+//  oh-my-password
+//
+//  Created by ChenYu Xiao on 12-4-20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "OMPAddMainUserViewController.h"
+
+@interface OMPAddMainUserViewController ()
+
+@end
+
+@implementation OMPAddMainUserViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
